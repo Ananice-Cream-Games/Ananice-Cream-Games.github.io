fa9de8cb0400ff32d3d92b7941a62de5940843d3
Sergueille <84402628+Sergueille@users.noreply.github.com>
2021-12-19 13:43:35 +0100

    Update canvas-scaler.js

--- canvas-scaler.js
+++ canvas-scaler.js
@@ -1,3 +1,3 @@
 let canvas = document.getElementById("unity-canvas");
 canvas.setAttribute("height", screen.availHeight);
-canvas.setAttribute("width", screen.availWidth);
+canvas.setAttribute("width", screen.availHeight * 1080 / 1920);
