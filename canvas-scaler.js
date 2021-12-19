b876327ca99a8ec0727b87297db3f5952fbd0891
Sergueille <84402628+Sergueille@users.noreply.github.com>
2021-12-19 13:51:37 +0100

    Test

--- canvas-scaler.js
+++ canvas-scaler.js
@@ -1,6 +1,6 @@
-document.onload(() => {
+setTimeout(() => {
     let canvas = document.getElementById("unity-canvas");
     let body = document.body
     canvas.setAttribute("height", body.style.height);
     canvas.setAttribute("width", body.style.height * 1080 / 1920);
-})
+}, 1000)
