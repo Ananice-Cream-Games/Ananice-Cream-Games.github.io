93fc8de3ea9cf6766b20c836dd3462c3db38351a
Sergueille <84402628+Sergueille@users.noreply.github.com>
2021-12-19 14:56:31 +0100

    canvas scale fix

--- canvas-scaler.js
+++ canvas-scaler.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
     let canvas = document.getElementById("unity-canvas");
-    let height = document.body.clientHeight
+    let height = document.documentElement.clientHeight
     canvas.setAttribute("height", height );
     canvas.setAttribute("width", height * 1080.0 / 1920.0);
 }, 1000)
